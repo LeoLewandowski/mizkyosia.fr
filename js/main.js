4455519c1fbdb447ccac7bf57a37a45276e6df6e
LeoLewandowski <leo.lewandowski@student.junia.com>
2023-09-30 19:06:14 +0200

    flexible grid implementation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('a.proj').forEach(a => a.onmouseover = 'this.t = textGen()');
+    checkSize();
+})
+
+window.addEventListener('resize', checkSize)
 
 let alpha = 45;
 setInterval(() => {
+    return;
     alpha = (alpha >= 360 ? 1 : alpha+1);
     document.body.style.setProperty('--bg-angle',`${alpha}deg`);
 }, 50);
+
+function textGen() {
+    String.fromCharCode()
+}
+
+function checkSize() {
+    if(window.screen.width <= 400) document.body.classList.add('smallScreen');
+    else document.body.classList.remove('smallScreen');
+}
